2f450c88b5d89f281fce544a643ab50f98e93ebc
Aamos Kekkonen <aamos.kekkonen@aalto.fi>
2024-01-07 02:13:08 +0200

    complete exercises 3.1-3.8

---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :response-time ms :body'))
+
+let notes = [
+    {
+      id: 1,
+      name: "Arto Hellas",
+      number: "040-123456"
+    },
+    {
+      id: 2,
+      name: "Ada Lovelace",
+      number: "39-44-5323523"
+    },
+    {
+      id: 3,
+      name: "Dan Abramov",
+      number: "12-43-234345"
+    },
+    {
+      id: 4,
+      name: "Mary Poppendick",
+      number: "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+      response.json(note)
+  } else {
+      response.status(404).end()
+  }
+})
+
+app.get('/api/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${notes.length} people</p><p>${new Date().toString()}</p>`)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const cap = 100000000
+  return Math.random(cap)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  if (notes.some(note => note.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  const note = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
